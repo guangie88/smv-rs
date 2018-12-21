d6457d6af12f4d1f720a63f8c006284091beacd5
Chen Weiguang <chen.weiguang@gmail.com>
2018-12-21 20:19:25 +0800

    Working string replacement impl

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,71 @@
 use failure::Error;
+use std::io::{self, Read};
+use std::str::FromStr;
 use structopt::StructOpt;
 
 mod semver;
 
-type Result<T> = std::result::Result<T, Error>;
+#[derive(Debug)]
+enum Input {
+    SemVer(semver::SemVer),
+    Stdin,
+}
+
+impl std::str::FromStr for Input {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        semver::SemVer::from_str(s).map(Input::SemVer).or_else(|e| {
+            if s == "-" {
+                Ok(Input::Stdin)
+            } else {
+                Err(e)
+            }
+        })
+    }
+}
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "Arguments to smv")]
 struct Args {
-    #[structopt(parse(try_from_str))]
-    semval: semver::SemVer,
+    #[structopt(
+        parse(try_from_str),
+        help = "x[.y.z] SemVer value to input, or - to use stdin"
+    )]
+    input: Input,
 
-    #[structopt()]
+    #[structopt(
+        help = "Emits string via replacement {x}=maj, {y}=min, {z}=patch"
+    )]
     emit: String,
 }
 
-fn main() -> Result<()> {
+fn main() -> Result<(), Error> {
     let args = Args::from_args();
 
-    print!("{}", args.emit
-        .replace('x', &format!("{}", args.semval.major))
-        .replace('y', &format!("{}", args.semval.minor.unwrap()))
-        .replace('z', &format!("{}", args.semval.patch.unwrap())));
+    let semval = match args.input {
+        Input::SemVer(semval) => semval,
+        Input::Stdin => {
+            let mut buf = String::new();
+            let stdin = io::stdin();
+            let mut handle = stdin.lock();
+            handle.read_to_string(&mut buf)?;
+            semver::SemVer::from_str(&buf)?
+        }
+    };
+
+    let x_rep = args.emit.replace("{x}", &format!("{}", semval.major));
+
+    let xy_rep = match semval.minor {
+        Some(minor) => x_rep.replace("{y}", &format!("{}", minor)),
+        None => x_rep,
+    };
+
+    let xyz_rep = match semval.patch {
+        Some(patch) => xy_rep.replace("{z}", &format!("{}", patch)),
+        None => xy_rep,
+    };
+
+    print!("{}", xyz_rep);
     Ok(())
 }
