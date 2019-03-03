3727df00e8a93bed2a348d377a6fda8d77b6bbd3
Chen Weiguang <chen.weiguang@gmail.com>
2019-03-03 21:09:31 +0800

    Also add unit tests for SemVer parsing

--- lib/src/semver.rs
+++ lib/src/semver.rs
@@ -62,3 +62,42 @@ impl std::str::FromStr for SemVer {
         })
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use std::str::FromStr;
+
+    #[test]
+    fn sem_ver_from_str_major() {
+        let sm = SemVer::from_str("3").unwrap();
+        assert_eq!(sm.major, 3);
+        assert_eq!(sm.minor, None);
+        assert_eq!(sm.patch, None);
+    }
+    
+    #[test]
+    fn sem_ver_from_str_major_minor() {
+        let sm = SemVer::from_str("3.1").unwrap();
+        assert_eq!(sm.major, 3);
+        assert_eq!(sm.minor, Some(1));
+        assert_eq!(sm.patch, None);
+    }
+
+    #[test]
+    fn sem_ver_from_str_major_minor_patch() {
+        let sm = SemVer::from_str("3.1.4").unwrap();
+        assert_eq!(sm.major, 3);
+        assert_eq!(sm.minor, Some(1));
+        assert_eq!(sm.patch, Some(4));
+    }
+
+    #[test]
+    fn sem_ver_from_str_large_major_minor_patch() {
+        let sm = SemVer::from_str("123.456.789").unwrap();
+        assert_eq!(sm.major, 123);
+        assert_eq!(sm.minor, Some(456));
+        assert_eq!(sm.patch, Some(789));
+    }
+}
