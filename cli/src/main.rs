89584d5a4fc2e8a0667f90d74a2fe3f105308f72
Chen Weiguang <chen.weiguang@gmail.com>
2019-03-03 21:30:51 +0800

    Place CLI implementation under subcommand

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -26,42 +26,65 @@ impl std::str::FromStr for Input {
 }
 
 #[derive(StructOpt, Debug)]
-#[structopt(name = "Arguments to smv")]
+#[structopt(name = "subcommand", about = "smv subcommands")]
+enum Subcommand {
+    #[structopt(name = "parse")]
+    Parse {
+        #[structopt(
+            parse(try_from_str),
+            help = "x[.y.z] SemVer value to input, or - to use stdin"
+        )]
+        input: Input,
+
+        #[structopt(
+            help = "Emits string via replacement x=maj, y=min, z=patch"
+        )]
+        template: String,
+
+        #[structopt(
+            short = "n",
+            help = "Insert newline char at the end if set"
+        )]
+        newline: bool,
+    },
+}
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "smv main arguments")]
 struct Args {
-    #[structopt(
-        parse(try_from_str),
-        help = "x[.y.z] SemVer value to input, or - to use stdin"
-    )]
-    input: Input,
-
-    #[structopt(help = "Emits string via replacement x=maj, y=min, z=patch")]
-    template: String,
-
-    #[structopt(short = "n")]
-    newline: bool,
+    #[structopt(subcommand)]
+    cmd: Subcommand,
 }
 
 fn main() -> Result<(), Error> {
     let args = Args::from_args();
 
-    let sem_ver = match args.input {
-        Input::SemVer(sem_ver) => sem_ver,
-        Input::Stdin => {
-            let mut buf = String::new();
-            let stdin = io::stdin();
-            let mut handle = stdin.lock();
-            handle.read_to_string(&mut buf)?;
-            SemVer::from_str(buf.trim_end())?
+    match args.cmd {
+        Subcommand::Parse {
+            input,
+            template,
+            newline,
+        } => {
+            let sem_ver = match input {
+                Input::SemVer(sem_ver) => sem_ver,
+                Input::Stdin => {
+                    let mut buf = String::new();
+                    let stdin = io::stdin();
+                    let mut handle = stdin.lock();
+                    handle.read_to_string(&mut buf)?;
+                    SemVer::from_str(buf.trim_end())?
+                }
+            };
+
+            let output = replace(&template, &sem_ver)?;
+
+            if !newline {
+                print!("{}", output);
+            } else {
+                println!("{}", output);
+            }
+
+            Ok(())
         }
-    };
-
-    let output = replace(&args.template, &sem_ver)?;
-
-    if !args.newline {
-        print!("{}", output);
-    } else {
-        println!("{}", output);
     }
-
-    Ok(())
 }
